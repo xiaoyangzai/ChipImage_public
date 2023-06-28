efdbc9d7bc2b011063f19cefd60f19c360715907
Yang Wang <wangyang.xjtu@foxmail.com>
2023-06-28 22:30:20 +0800

    Enable Auto foucs adjustation mock function. Enable the callback function to retrieve the image from host app.
    Signed-off-by: Yang Wang <wangyang.xjtu@foxmail.com>

---
+++ imageProcessAlgor/focusQuality.cpp
@@ -0,0 +1,47 @@
+#include <opencv2/opencv.hpp>
+#include <windows.h>
+#include <wincrypt.h>
+#include <iostream>
+#include <stdio.h>
+#include "cutImageAlgr.h"
+#include "focusQuality.h"
+using namespace cv;
+using namespace std;
+#define MEM_MAX_SIZE 1024*1024*15 
+static char *g_dynamicMem = NULL;
+extern "C"
+{
+
+    __declspec(dllexport) int AutoFocus(int min_focus, int max_focus, int step, CaptureImage captureImage) {
+		int quality = -1;
+		char msg[256] = "";
+		sprintf_s(msg, sizeof(msg) - strlen(msg), "AutoFocus:\nMax: %d, Min: %d, Step: %d\n", min_focus, max_focus, step);
+        if (captureImage == NULL) {
+		    sprintf_s(msg, sizeof(msg) - strlen(msg), "Empty image capture handler. Please check again!\n");
+		    DebugPrint(msg);
+            return -1;
+        }
+        if (!g_dynamicMem) {
+            g_dynamicMem = (char*)malloc(MEM_MAX_SIZE * sizeof(char));
+        }
+        int length = -1;
+        if (captureImage(g_dynamicMem, length, 5) < 0) {
+		    sprintf_s(msg, sizeof(msg) - strlen(msg), "Invaild image capture handler. Please check again!\n");
+		    DebugPrint(msg);
+            return -1;
+        }
+		std::string imageData = Base64Decoder(g_dynamicMem, length);
+		std::vector<uchar> decodedImage(imageData.begin(), imageData.end());
+		cv::Mat imageMat = imdecode(decodedImage, cv::IMREAD_COLOR);
+        quality = FocusQuality(imageMat);
+		sprintf_s(msg, sizeof(msg) - strlen(msg), "[AutoFocus] Received data length: %d\nQuality: %d\n",length, quality);
+		DebugPrint(msg);
+        free(g_dynamicMem);
+		return 0;
+	}
+    __declspec(dllexport) int FocusQuality(cv::Mat& image) {
+        int quality = -1;
+        quality = 78;
+        return quality;
+    }
+}
---
+++ imageProcessAlgor/focusQuality.h
@@ -0,0 +1,22 @@
+#pragma once
+
+#include <iostream>
+#include <opencv2/opencv.hpp>
+using namespace cv;
+extern "C"
+{
+    typedef int (*CaptureImage)(char *, int &, int);
+    /**
+     * @brief calculate the quality on specified focus value
+     * @param min_focus                 the minimum value of focus
+     * @param max_focus                 the maximum value of focus
+     * @param step                      the value for each step to adjust focus
+     * @param captureImage              the function to capture the image
+     * @return return 0 if successful, otherwise non-zero will return.
+     */
+    __declspec(dllexport) int AutoFocus(int min_focus,
+                                        int max_focus,
+                                        int step,
+                                        CaptureImage captureImage);
+    __declspec(dllexport) int FocusQuality(cv::Mat& image);
+}
