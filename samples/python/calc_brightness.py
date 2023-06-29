47a4fa13e1ca8ede6684e27b2fdec168c654b1c2
deyao.yang <deyao.yang@momenta.ai>
2023-06-29 18:43:07 +0800

    add 3 kind of calculating sharpness funcs into img_proc

--- samples/python/calc_brightness.py
+++ samples/python/calc_brightness.py
@@ -5,10 +5,16 @@ from img_proc import *
 
 def main():
     img = cv2.imread(sys.argv[1])
+    print('calculating brightness:')
     print(stat_brightness_mean(img))
     print(stat_brightness_RMS(img))
     print(stat_brightness_formula(img))
     print(stat_brightness_RMS_formula(img))
+
+    print('calculating sharpness:')
+    print(stat_sharpness_Tenengrad(img, threshold=100))
+    print(stat_sharpness_Laplacian(img))
+    print(stat_sharpness_Variance(img))
 
 
 if __name__ == '__main__':
--- samples/python/img_proc/core.py
+++ samples/python/img_proc/core.py
@@ -32,3 +32,29 @@ def stat_brightness_RMS_formula(img):
     rms = (img*img).mean(0).mean(0)[::-1]
     brightness = np.sqrt((0.241 * rms[0] + 0.691 * rms[1] + 0.068 * rms[2]))
     return brightness
+
+
+def stat_sharpness_Tenengrad(img, threshold=500):
+    if img.shape[-1] == 3:
+        img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    img = img.astype(float)
+    gx = cv2.Sobel(img, cv2.CV_64F, 1, 0, ksize=3)
+    gy = cv2.Sobel(img, cv2.CV_64F, 0, 1, ksize=3)
+    fm = gx**2 + gy**2
+    sharpness = (fm>threshold).mean()
+    return sharpness
+
+
+def stat_sharpness_Laplacian(img):
+    if img.shape[-1] == 3:
+        img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    img = img.astype(float)
+    lap = cv2.Laplacian(img, cv2.CV_64F)
+    return np.var(lap)
+
+
+def stat_sharpness_Variance(img):
+    if img.shape[-1] == 3:
+        img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    img = img.astype(float)
+    return np.var(img)
